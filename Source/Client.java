cd2b1863f0734cf561d92d637205e88409069c18
aidan <aidan.campbell@gmail.com>
2014-10-02 15:56:39 -0400

    functionally moving data around sockets.

--- Source/Client.java
+++ Source/Client.java
@@ -1,24 +1,27 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.*;
-import java.lang.Exception;
-import java.lang.SuppressWarnings;
-import java.lang.System;
+import java.net.Socket;
 
 public class Client {
 
     public static void main(String[] args) {
         try {
+            System.out.println("Began. connecting to socket.");
             Socket clientSocket = new Socket("localhost", 5005);
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            System.out.println("Connected to socket. preparing to make request");
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            outToServer.writeBytes("hello");
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            System.out.println("Opened request.");
+            outToServer.writeBytes("hello \n");
+            outToServer.flush();
+            System.out.println("Made request, waiting for response.");
             System.out.println(inFromServer.readLine());
             clientSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Complete");
     }//empty main method to initialize program
 
 
--- Source/Server.java
+++ Source/Server.java
@@ -23,29 +23,35 @@ Use byte buffers because you may be handling binaries
 flush sockets(?)
  */
 
-import java.io.*;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.lang.Exception;
-import java.lang.SuppressWarnings;
-import java.lang.System;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server{
 
     public static void main(String[] args){
         try {
             ServerSocket welcomeSocket = new ServerSocket(5005);
+            System.out.println("Opened welcome socket, waiting for client");
             while (true) {
                 Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("connection accepted");
                 BufferedReader input = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                System.out.println("Got input. writing output.");
                 DataOutputStream output = new DataOutputStream(connectionSocket.getOutputStream());
-                output.writeBytes(input.toString().toUpperCase());
+                System.out.println("Got output stream.  Writing to it.");
+
+                output.writeBytes(input.readLine().toUpperCase()+'\n');
+
+                System.out.println("done doing stuff. closing.");
+                connectionSocket.close();
             }
         } catch(Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
+        System.out.println("Finished.");
     }//empty main method to initialize program
 
 }

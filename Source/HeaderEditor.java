3bf4f01572a940e950772313b2c49076040d76ba
aidan <aidan.campbell@gmail.com>
2014-11-01 14:06:31 -0400

    removed many unused methods

--- Source/HeaderEditor.java
+++ Source/HeaderEditor.java
@@ -5,34 +5,6 @@ import java.util.regex.Pattern;
  */
 public class HeaderEditor {
 
-    private enum TERMINATION_STAGE {reset,r1,n1,r2}
-    private TERMINATION_STAGE termination_stage = TERMINATION_STAGE.reset;
-
-    public static int parseLength(String header){
-        if(header == null) return -1;
-        int returnVar = 0;
-        Pattern pattern = Pattern.compile("Content-Length:(.)*");
-        Matcher matcher = pattern.matcher(header);
-        if(!matcher.find()){
-            //it was a GET, or non-payload HTTP packet
-            return -1;
-        }
-        String length = matcher.group();
-        length = length.replaceFirst("Content-Length: ","");
-        length = length.trim();
-        try {
-            returnVar = Integer.parseInt(length);
-        } catch(NumberFormatException e){
-            System.err.println("Error while parsing '"+length+"' to an integer!");
-        }
-        return returnVar;
-    }
-
-    public static int parseLength(byte[] request){
-        String strRequest = new String(request);
-        return parseLength(strRequest);
-    }
-
     /**
      * Grabs the host from the HTTP request
      * @param request HTTP request as a string
@@ -64,71 +36,24 @@ public class HeaderEditor {
         return returnVar.getBytes();
     }
 
-    public static int getHeaderEnd(byte[] header){//0x0D0A == \r\n
-        int returnVar = -1;
-        TERMINATION_STAGE termination_stage = TERMINATION_STAGE.reset;
-        for(int i = 0; i< header.length; i++) {//0D0A
-            byte b = header[i];
-            switch (termination_stage){
-                case reset:
-                    if (b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                    break;
-                case r1:
-                    if(b == 0x0A) termination_stage = TERMINATION_STAGE.n1;
-                    if(b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                    if(b != 0x0D && b != 0x0A) termination_stage = TERMINATION_STAGE.reset;
-                    break;
-                case n1:
-                    if(b == 0x0D) termination_stage = TERMINATION_STAGE.r2;
-                    if(b == 0x0A) termination_stage = TERMINATION_STAGE.r1;
-                    if(b != 0x0D && b != 0x0A) termination_stage = TERMINATION_STAGE.reset;
-                    break;
-                case r2:
-                    if(b == 0x0A) return i;
-                    if(b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                    else termination_stage = TERMINATION_STAGE.reset;
-                    break;
-            }
-        }
-        return returnVar;
-    }
-
-    public boolean isHeaderEnded(char c){
-        byte b = (byte) c;
-        switch (termination_stage){
-            case reset:
-                if (b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                break;
-            case r1:
-                if(b == 0x0A) termination_stage = TERMINATION_STAGE.n1;
-                if(b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                if(b != 0x0D && b != 0x0A) termination_stage = TERMINATION_STAGE.reset;
-                break;
-            case n1:
-                if(b == 0x0D) termination_stage = TERMINATION_STAGE.r2;
-                if(b == 0x0A) termination_stage = TERMINATION_STAGE.r1;
-                if(b != 0x0D && b != 0x0A) termination_stage = TERMINATION_STAGE.reset;
-                break;
-            case r2:
-                if(b == 0x0A) return true;
-                if(b == 0x0D) termination_stage = TERMINATION_STAGE.r1;
-                else termination_stage = TERMINATION_STAGE.reset;
-                break;
-        }
-        return false;
-    }
-
-    public static byte[] addForwardHeader(byte[] request, String clientIP){
+    public static byte[] toggleForwardHeader(byte[] request, String clientIP){
         if(request == null) return null;
         StringBuffer returnVar = new StringBuffer(new String(request));
-        returnVar = new StringBuffer(returnVar.toString().replaceFirst("X-Forwarded-For: ","X-Forwarded-For: "+clientIP));
+        returnVar = new StringBuffer(returnVar.toString().replaceFirst("X-Forwarded-For: ","X-Forwarded-For: "+clientIP.substring(1)));
         if(!returnVar.toString().contains("X-Forwarded-For: ")){
             int index = 0;
             while(returnVar.charAt(index) != '\n') {//AOOBE
                 index++;
+                if(index >= returnVar.length()){
+                    //we've got problems
+                }
             }
             index++;//go just past the CRLF
-            returnVar.insert(index, "X-Forwarded-For: "+clientIP+'\n');
+            returnVar.insert(index, "X-Forwarded-For: "+clientIP.substring(1)+'\n');
+        } else {
+            String temp = returnVar.toString();
+            temp = temp.replace("X-Forwarded-For: "+clientIP,"");
+            returnVar = new StringBuffer(temp);
         }
         return returnVar.toString().getBytes();
     }
